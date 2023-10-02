1ff2dd8f50bbbf72ca3ba3e13ab6ebc063bf2cd5
Md Aatif Tegginmani <mdaatif84@gmail.com>
2023-10-02 22:56:33 +0530

    new project

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const  colors =require("colors") 
+
+
+
+const {chats} =require("./data/data")
+const dotenv = require("dotenv")
+const connectDB = require('./config/db')
+
+
+dotenv.config()
+connectDB()
+const app = express()
+
+
+app.get('/', (req,res)=>{
+    res.send("App is running")
+
+})
+
+app.get("/api/chat" , (req,res)=>{
+    res.send(chats)
+})
+
+app.get("/api/chat/:id",(req , res)=>{
+    // console.log(req.params.id)
+    const singleChat = chats.find(c=>c._id === req.params.id)
+    res.send(singleChat)
+})
+
+
+const PORT = process.env.PORT || 5000
+
+
+
+app.listen(`${PORT}`,console.log(`server is started on port ${PORT}`.yellow.bold))
