f4864baa3265b6ddf356fe80ccd7ab1764a5850d
Md Aatif Tegginmani <mdaatif84@gmail.com>
2023-10-07 20:53:46 +0530

    register user func added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const  colors =require("colors") 
 
+const userRoutes = require('./routes/userRoutes')
 
 
-const {chats} =require("./data/data")
+// const {chats} =require("./data/data")
 const dotenv = require("dotenv")
 const connectDB = require('./config/db')
 
@@ -13,20 +14,28 @@ connectDB()
 const app = express()
 
 
+app.use(express.json()) ;
+
+
+
 app.get('/', (req,res)=>{
     res.send("App is running")
 
 })
 
-app.get("/api/chat" , (req,res)=>{
-    res.send(chats)
-})
+app.use('/api/user',userRoutes)
 
-app.get("/api/chat/:id",(req , res)=>{
-    // console.log(req.params.id)
-    const singleChat = chats.find(c=>c._id === req.params.id)
-    res.send(singleChat)
-})
+
+
+// app.get("/api/chat" , (req,res)=>{
+//     res.send(chats)
+// })
+
+// app.get("/api/chat/:id",(req , res)=>{
+//     // console.log(req.params.id)
+//     const singleChat = chats.find(c=>c._id === req.params.id)
+//     res.send(singleChat)
+// })
 
 
 const PORT = process.env.PORT || 5000
